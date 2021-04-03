9d1976f9d1676d62cac3732af24714069278df41
ranningw <ranningwang@outlook.com>
2021-04-03 23:55:33 +0200

    First Java program

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,10 @@
 public class HelloWorld{
 
     public static void main(String[] args) {
-        system.out.println("HW!");
+        // System.out.println("HW!");
+        System.out.println("HW!");
+        
     }
+
+   
 }
